63d73e2f8232868ccf69dc60bb4d96fe3bfd0d47
Elena Solomko <lena06051981be@gmail.com>
2022-10-03 13:31:39 +0300

    mobile touch

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ function parallax(event) {
     layer.style.transform = `translateX(${(event.clientX * speed) / 2000}px)`;
   });
 }
+
 document.addEventListener("mousemove", parallax);
+document.addEventListener("touchmove", parallaxTouch); // yoo-hoo!!))) It`s works!
+
+function parallaxTouch(event) {
+  this.querySelectorAll(".layer").forEach((layer) => {
+    let speed = layer.getAttribute("data-speed");
+    layer.style.transform = `translateX(${
+      (event.changedTouches[0].clientX * speed) / 1000
+      // (event.changedTouches[0].pageX * speed) / 1000
+    }px)`;
+  });
+}
